60795b5ccbfb0c850b19ca0a4c7ebd92617036d9
Javier Muñoz Parra <jmunpar1307@g.educaand.es>
2025-11-07 19:05:56 +0100

    Ejercicios-Relacion5-Java

---
+++ Relacion5-Variables/TiposVariables3.java
@@ -0,0 +1,41 @@
+/*Ejercicio 3 Convierte en expresiones algorítmicas con Java las siguientes expresiones algebraicas. Coloca
+paréntesis solamente donde sean necesarios.
+
+1º DAW | Javier Muñoz Parra */
+
+public class TiposVariables3{
+    public static void main ( String args[]){
+        /* a) */
+        double sumafrac = 3/2+4/3;
+
+        /* b) */
+        double restaFracComp = 1/(x-5) - 3*x*y/4;
+
+        /* c) */
+        double sumafrac2 = 1/2 + 7;
+
+        /* d) */
+        double sumafrac3 = 7 + 1/2;
+
+        /* e) */
+        double sumafrac4 = (a*a/(b - c) + (d - e)/(f-g*h/j));
+
+        /* f) */
+        double sumafrac5 = m/n + p 
+
+        /* g) */
+        double sumafrac6 =  m + n/(p - q)
+
+        /* h) */
+        double sumafrac7 = a * a/(b * b) + c * c / (d*d)
+
+        /* i) */
+        double sumafrac8 = (m + n/p) /(q -r/s)
+
+        /* j) */ 
+        double sumafrac9 = (3a + b)/(c - (d + 5e /(f + g/2*h)));
+
+        /* k) */
+        double sumafrac10 = (a* a + 2 * a * b)/(1/(x * x))+2;
+    }
+}
---
+++ Relacion5-Variables/TiposVariables4.java
@@ -0,0 +1,37 @@
+public class TiposVariables4 {
+    public static void main(String[] args) {
+        /*Definicion de variables*/
+        int x = 1;
+        int y = 1;
+        double n = 2;
+        double m = 2;
+
+        /*Programa Principal*/
+        
+        /*Operaciones de x e y*/
+        System.out.println("La suma de x + y es: "+(x+y));
+        System.out.println("La diferecia de x - y es :"+(x-y));
+        System.out.println("El producto de x * y es: "+(x * y));
+        System.out.println("El cociente de x / y es: "+(x / y));
+        System.out.println("El resto de x % y es: "+ (x % y));
+
+        /* Operaciones de n + m */
+        System.out.println("La suma de n + m es: "+ (n + m));
+        System.out,println("La diferencia de n - m es: "+ (n - m));
+        System.out.println("El producto de n * m es: "+ (n * m));     
+        System.out.println("El cociente de n / m es: "+(n / m));
+        System.out.println("El resto de n % m es: " + (n % m));
+
+        /*Operaciones combinando varias variables*/
+        System.out.println("La suma de x + n es "+ (x + n));
+        System.out.println("El cociente de y / m"+ (y / m));
+        System.out.println("El resto de y % m es: "+ (y % m));
+
+        /*Operaciones con todas las variables*/
+        System.out.println("El doble de x es: "+ (x * 2));
+        System.out.println("El doble de y es: "+(y * 2));
+        System.out.println("El doble de n es: "+(n * 2));
+        System.out.println("El doble de n es: "+(m * 2));
+    })
+
+}
